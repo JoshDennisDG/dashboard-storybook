c36bd74f25e278d10c8b303bdb6ac1f7838d9d8f
JoshDennisDG <joshua@digitalgenius.com>
2023-07-17 11:09:14 +0100

    Removing files

--- src/utils/getThemingArgTypes.ts
+++
@@ -1,6 +0,0 @@
-import { Theme } from "@chakra-ui/react";
-import theme from "../theme";
-import { getThemingArgTypes } from "@chakra-ui/storybook-addon";
-
-export default (componentName: any) =>
-  getThemingArgTypes(theme as Theme, componentName);
